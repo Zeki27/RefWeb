fffde3b1a96b7cd160150abc2ae6045b9d634ceb
Zsolt S.Nagy <snagyzsolt97@gmail.com>
2021-01-27 11:54:15 +0100

    Add project files.

---
+++ RefWeb/Controllers/LanguageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RefWeb.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Change(String language)
+        {
+            if (!String.IsNullOrEmpty(language))
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+            HttpCookie cookie = new HttpCookie("Languages");
+            cookie.Value = language;
+            Response.Cookies.Add(cookie);
+            return View("Index");
+        }
+    }
+}
---
+++ RefWeb/Controllers/PageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RefWeb.Controllers
+{
+    public class PageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SoftwarePage()
+        {
+            return View();
+        }
+
+        public ActionResult WebPage()
+        {
+            return View();
+        }
+
+        public ActionResult GamePage()
+        {
+            return View();
+        }
+
+    }
+}
