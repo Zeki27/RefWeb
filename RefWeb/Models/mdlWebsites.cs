7f965cc929e1c1f542291328705d521a75768770
Zsolt S.Nagy <snagyzsolt97@gmail.com>
2021-02-03 15:13:00 +0100

    pass param with url

---
+++ RefWeb/Models/mdlWebsites.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RefWeb.Models
+{
+    public class mdlWebsites
+    {
+        public static int WebsiteId { get; private set; }
+        public static string WebsiteName { get; private set; }
+        public static string WebsiteDescription { get; private set; }
+        public static string WebsiteURL { get; private set; }
+
+        public mdlWebsites(int wid, string wname, string wdesc, string wurl)
+        {
+            WebsiteId = wid;
+            WebsiteName = wname;
+            WebsiteDescription = wdesc;
+            WebsiteURL = wurl;
+        }
+    }
+}
