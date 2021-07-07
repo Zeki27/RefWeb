3f7200e0690dfeddc4b9222b924fc478c2a69246
Zsolt S.Nagy <snagyzsolt97@gmail.com>
2021-07-07 12:09:35 +0200

    update SQL file for phpmyadmin

--- RefWeb/_SQLDatabase/myrefappdb.sql
+++ RefWeb/_SQLDatabase/myrefappdb.sql
@@ -2,10 +2,10 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Gép: 127.0.0.1
--- Létrehozás ideje: 2021. Máj 25. 14:16
--- Kiszolgáló verziója: 10.4.19-MariaDB
--- PHP verzió: 8.0.6
+-- Host: 127.0.0.1
+-- Generation Time: Jul 07, 2021 at 12:07 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `myrefappdb`
+-- Database: `myrefappdb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `myrefappdrivlic`
+-- Table structure for table `myrefappdrivlic`
 --
 
 CREATE TABLE `myrefappdrivlic` (
@@ -35,10 +35,18 @@ CREATE TABLE `myrefappdrivlic` (
   `dl_uid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `myrefappdrivlic`
+--
+
+INSERT INTO `myrefappdrivlic` (`dl_id`, `dl_cat`, `dl_acdate`, `dl_expdate`, `dl_uid`) VALUES
+(1, 'B', '2016-04-20', '2025-02-27', 1),
+(2, 'A1', '2019-08-01', '2025-02-27', 1);
+
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `myrefappeduc`
+-- Table structure for table `myrefappeduc`
 --
 
 CREATE TABLE `myrefappeduc` (
@@ -52,10 +60,19 @@ CREATE TABLE `myrefappeduc` (
   `edu_uid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `myrefappeduc`
+--
+
+INSERT INTO `myrefappeduc` (`edu_id`, `edu_startdate`, `edu_enddate`, `edu_schoolnamehun`, `edu_schoolnameeng`, `edu_eduhun`, `edu_edueng`, `edu_uid`) VALUES
+(1, '2017.09. ', '2019.10. ', 'Pécsi SZC Simonyi Károly Szakgimnáziuma és Szakközépiskola ', 'Pécsi SZC Simonyi Károly Szakgimnáziuma és Szakközépiskola ', 'szoftverfejlesztő (OKJ 54-213-05)', 'software developer (OKJ 54-213-05)', 1),
+(5, '2016.09.', '2017.05.', 'PSZC Mohácsi Radnóti Miklós Szakgimnáziuma és Szakközépiskolája', 'PSZC Mohácsi Radnóti Miklós Szakgimnáziuma és Szakközépiskolája', 'hegesztő', 'welder', 1),
+(6, '2011.09.', '2015.07.', 'PSZC Mohácsi Radnóti Miklós Szakgimnáziuma és Szakközépiskolája rendészet', 'PSZC Mohácsi Radnóti Miklós Szakgimnáziuma és Szakközépiskolája rendészet', 'érettségi', 'graduation', 1);
+
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `myrefappexp`
+-- Table structure for table `myrefappexp`
 --
 
 CREATE TABLE `myrefappexp` (
@@ -65,10 +82,19 @@ CREATE TABLE `myrefappexp` (
   `exp_uid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `myrefappexp`
+--
+
+INSERT INTO `myrefappexp` (`exp_id`, `exp_deschun`, `exp_desceng`, `exp_uid`) VALUES
+(1, 'C#, ASP.Net, MySQL ', 'C#, ASP.Net, MySQL ', 1),
+(2, 'Visual Studio 2019, Unity3d, Blender 2.8 ', 'Visual Studio 2019, Unity3d, Blender 2.8 ', 1),
+(3, 'GitHub, Bitbucket verziókezelő ', 'GitHub, Bitbucket verziókezelő ', 1);
+
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `myrefapplang`
+-- Table structure for table `myrefapplang`
 --
 
 CREATE TABLE `myrefapplang` (
@@ -80,10 +106,17 @@ CREATE TABLE `myrefapplang` (
   `lang_uid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `myrefapplang`
+--
+
+INSERT INTO `myrefapplang` (`lang_id`, `lang_namehun`, `lang_nameeng`, `lang_levelhun`, `lang_leveleng`, `lang_uid`) VALUES
+(1, 'Angol', 'English', 'haladó (beszéd & írás)', 'advanced (speach & writing)', 1);
+
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `myrefappnews`
+-- Table structure for table `myrefappnews`
 --
 
 CREATE TABLE `myrefappnews` (
@@ -97,16 +130,17 @@ CREATE TABLE `myrefappnews` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- A tábla adatainak kiíratása `myrefappnews`
+-- Dumping data for table `myrefappnews`
 --
 
 INSERT INTO `myrefappnews` (`myrefappnewsid`, `myrefappnewsnamehun`, `myrefappnewsnameeng`, `myrefappnewsdate`, `myrefappnewsdeschun`, `myrefappnewsdesceng`, `myrefappnewsuserid`) VALUES
-(1, 'Első', 'First', '2021-02-05', 'Hír', 'News', 1);
+(1, 'Első', 'First', '2021-02-05', 'Hír', 'News', 1),
+(2, 'Második Teszt Hír', 'Secon Test News', '2021-07-01', 'Teszt2', 'Test2', 1);
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `myrefapppersonaldesc`
+-- Table structure for table `myrefapppersonaldesc`
 --
 
 CREATE TABLE `myrefapppersonaldesc` (
@@ -116,10 +150,17 @@ CREATE TABLE `myrefapppersonaldesc` (
   `pd_uid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `myrefapppersonaldesc`
+--
+
+INSERT INTO `myrefapppersonaldesc` (`pd_id`, `pd_deschun`, `pd_desceng`, `pd_uid`) VALUES
+(1, 'Szabadidőmet többnyire sporttal, motorozással és saját projektjeim fejlesztésével (használt szoftverek: Visual Studio, Unity, Blender, Visual Studio Code) és egyéb hobbim űzésével töltöm. Számomra hobbi és szórakozás az informatika, minden téren érdekel. Szívesen tanulok és fejlődök. Érdekelnek az újdonságok és az új technológiák. Törekszem az igényes és precíz munkára. Ha munkáról van szó céltudatos és motivált vagyok. Szeretem a kihívásokat és úgy gondolom, jó problémamegoldó képességgel rendelkezem. A szakmát igyekszem angol nyelven tanulni illetve ahol csak tudom fejleszteni az angol nyelvtudásom (pl.: Udemy, Stackoverflow). Eddig főként C# nyelven programozást tanultam/tanulok. Java-t és PHP-t (CodeIgniter, Phalcon) írtam már viszont nincs bennük nagyon tapasztalatom, viszont szívesen megtanulok más nyelveket is. Hálózat terén is rendelkezem alapszintű ismeretekkel (pl.: MikroTik, hálózatépítés, konfigurálás).  ', 'I usually spend my free time working out, riding motorcycle and developing my own project’s (used software’s: Visual Studio, Unity, Blender, Visual Studio Code). For me, hobbie and entertainment the IT, I am interested in all areas. I like to learn and develop myself. I am interested in new technologies and programming languages. I strive for demanding and precise work. When it comes to work, I am purposeful and motivated. I love challenges and I think I have a good problem solving skill. I try to learn the profession in English and wherever I can improve my English skills (eg Udemy, Stackoverflow) I do. So far, I have mainly studied programming in C # but I’m interested in other programing language as well. I wrote Java and PHP (CodeIgniter, Phalcon) before but I don\'t have much experience with them. I also have some basic knowledge in the field of networking and IT system administrator (eg: MikroTik, networking, configuration).', 1);
+
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `myrefapppersonalinf`
+-- Table structure for table `myrefapppersonalinf`
 --
 
 CREATE TABLE `myrefapppersonalinf` (
@@ -133,10 +174,17 @@ CREATE TABLE `myrefapppersonalinf` (
   `pi_uid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `myrefapppersonalinf`
+--
+
+INSERT INTO `myrefapppersonalinf` (`pi_id`, `pi_name`, `pi_birthp`, `pi_birtht`, `pi_address`, `pi_phonen`, `pi_email`, `pi_uid`) VALUES
+(1, 'S. Nagy Zsolt', 'Mohács', '1997-02-27', '7759 Lánycsók Dózsa utca 36', '+36 30 188 6067', 'snagyzsolt97@gmail.com', 1);
+
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `myrefapproles`
+-- Table structure for table `myrefapproles`
 --
 
 CREATE TABLE `myrefapproles` (
@@ -145,7 +193,7 @@ CREATE TABLE `myrefapproles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- A tábla adatainak kiíratása `myrefapproles`
+-- Dumping data for table `myrefapproles`
 --
 
 INSERT INTO `myrefapproles` (`myrefapprole_id`, `myrefapprole_name`) VALUES
@@ -156,7 +204,7 @@ INSERT INTO `myrefapproles` (`myrefapprole_id`, `myrefapprole_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `myrefappusers`
+-- Table structure for table `myrefappusers`
 --
 
 CREATE TABLE `myrefappusers` (
@@ -168,7 +216,7 @@ CREATE TABLE `myrefappusers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- A tábla adatainak kiíratása `myrefappusers`
+-- Dumping data for table `myrefappusers`
 --
 
 INSERT INTO `myrefappusers` (`user_id`, `username`, `user_e_mail`, `password`, `myrefrole_id`) VALUES
@@ -177,7 +225,7 @@ INSERT INTO `myrefappusers` (`user_id`, `username`, `user_e_mail`, `password`, `
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `myrefappworkp`
+-- Table structure for table `myrefappworkp`
 --
 
 CREATE TABLE `myrefappworkp` (
@@ -192,196 +240,204 @@ CREATE TABLE `myrefappworkp` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexek a kiírt táblákhoz
+-- Dumping data for table `myrefappworkp`
+--
+
+INSERT INTO `myrefappworkp` (`wp_id`, `wp_startdate`, `wp_enddate`, `wp_wpnamehun`, `wp_wpnameeng`, `wp_rolehun`, `wp_roleeng`, `wp_uid`) VALUES
+(1, '2020.07.', 'jelenleg / now', 'VIDIAN Kft.', 'VIDIAN Kft.', 'rendszergazda', 'IT system administrator', 1),
+(2, '2019.12.', '2020.06.', 'Mohács Térségi Általános Iskola ', 'Mohács Térségi Általános Iskola ', 'rendszergazda', 'IT system administrator', 1);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- A tábla indexei `myrefappdrivlic`
+-- Indexes for table `myrefappdrivlic`
 --
 ALTER TABLE `myrefappdrivlic`
   ADD PRIMARY KEY (`dl_id`),
   ADD KEY `dl_uid` (`dl_uid`);
 
 --
--- A tábla indexei `myrefappeduc`
+-- Indexes for table `myrefappeduc`
 --
 ALTER TABLE `myrefappeduc`
   ADD PRIMARY KEY (`edu_id`),
   ADD KEY `edu_uid` (`edu_uid`);
 
 --
--- A tábla indexei `myrefappexp`
+-- Indexes for table `myrefappexp`
 --
 ALTER TABLE `myrefappexp`
   ADD PRIMARY KEY (`exp_id`),
   ADD KEY `exp_uid` (`exp_uid`);
 
 --
--- A tábla indexei `myrefapplang`
+-- Indexes for table `myrefapplang`
 --
 ALTER TABLE `myrefapplang`
   ADD PRIMARY KEY (`lang_id`),
   ADD KEY `lang_uid` (`lang_uid`);
 
 --
--- A tábla indexei `myrefappnews`
+-- Indexes for table `myrefappnews`
 --
 ALTER TABLE `myrefappnews`
   ADD PRIMARY KEY (`myrefappnewsid`),
   ADD KEY `myrefappnewsuserid` (`myrefappnewsuserid`);
 
 --
--- A tábla indexei `myrefapppersonaldesc`
+-- Indexes for table `myrefapppersonaldesc`
 --
 ALTER TABLE `myrefapppersonaldesc`
   ADD PRIMARY KEY (`pd_id`),
   ADD KEY `pd_uid` (`pd_uid`);
 
 --
--- A tábla indexei `myrefapppersonalinf`
+-- Indexes for table `myrefapppersonalinf`
 --
 ALTER TABLE `myrefapppersonalinf`
   ADD PRIMARY KEY (`pi_id`),
   ADD KEY `pi_uid` (`pi_uid`);
 
 --
--- A tábla indexei `myrefapproles`
+-- Indexes for table `myrefapproles`
 --
 ALTER TABLE `myrefapproles`
   ADD PRIMARY KEY (`myrefapprole_id`);
 
 --
--- A tábla indexei `myrefappusers`
+-- Indexes for table `myrefappusers`
 --
 ALTER TABLE `myrefappusers`
   ADD PRIMARY KEY (`user_id`),
   ADD KEY `myrefrole_id` (`myrefrole_id`);
 
 --
--- A tábla indexei `myrefappworkp`
+-- Indexes for table `myrefappworkp`
 --
 ALTER TABLE `myrefappworkp`
   ADD PRIMARY KEY (`wp_id`),
   ADD KEY `wp_uid` (`wp_uid`);
 
 --
--- A kiírt táblák AUTO_INCREMENT értéke
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT a táblához `myrefappdrivlic`
+-- AUTO_INCREMENT for table `myrefappdrivlic`
 --
 ALTER TABLE `myrefappdrivlic`
-  MODIFY `dl_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `dl_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT a táblához `myrefappeduc`
+-- AUTO_INCREMENT for table `myrefappeduc`
 --
 ALTER TABLE `myrefappeduc`
-  MODIFY `edu_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `edu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT a táblához `myrefappexp`
+-- AUTO_INCREMENT for table `myrefappexp`
 --
 ALTER TABLE `myrefappexp`
-  MODIFY `exp_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `exp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT a táblához `myrefapplang`
+-- AUTO_INCREMENT for table `myrefapplang`
 --
 ALTER TABLE `myrefapplang`
-  MODIFY `lang_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `lang_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT a táblához `myrefappnews`
+-- AUTO_INCREMENT for table `myrefappnews`
 --
 ALTER TABLE `myrefappnews`
-  MODIFY `myrefappnewsid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `myrefappnewsid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT a táblához `myrefapppersonaldesc`
+-- AUTO_INCREMENT for table `myrefapppersonaldesc`
 --
 ALTER TABLE `myrefapppersonaldesc`
-  MODIFY `pd_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `pd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT a táblához `myrefapppersonalinf`
+-- AUTO_INCREMENT for table `myrefapppersonalinf`
 --
 ALTER TABLE `myrefapppersonalinf`
-  MODIFY `pi_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `pi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT a táblához `myrefapproles`
+-- AUTO_INCREMENT for table `myrefapproles`
 --
 ALTER TABLE `myrefapproles`
   MODIFY `myrefapprole_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT a táblához `myrefappusers`
+-- AUTO_INCREMENT for table `myrefappusers`
 --
 ALTER TABLE `myrefappusers`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT a táblához `myrefappworkp`
+-- AUTO_INCREMENT for table `myrefappworkp`
 --
 ALTER TABLE `myrefappworkp`
-  MODIFY `wp_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `wp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Megkötések a kiírt táblákhoz
+-- Constraints for dumped tables
 --
 
 --
--- Megkötések a táblához `myrefappdrivlic`
+-- Constraints for table `myrefappdrivlic`
 --
 ALTER TABLE `myrefappdrivlic`
   ADD CONSTRAINT `myrefappdrivlic_ibfk_1` FOREIGN KEY (`dl_uid`) REFERENCES `myrefappusers` (`user_id`);
 
 --
--- Megkötések a táblához `myrefappeduc`
+-- Constraints for table `myrefappeduc`
 --
 ALTER TABLE `myrefappeduc`
   ADD CONSTRAINT `myrefappeduc_ibfk_1` FOREIGN KEY (`edu_uid`) REFERENCES `myrefappusers` (`user_id`);
 
 --
--- Megkötések a táblához `myrefappexp`
+-- Constraints for table `myrefappexp`
 --
 ALTER TABLE `myrefappexp`
   ADD CONSTRAINT `myrefappexp_ibfk_1` FOREIGN KEY (`exp_uid`) REFERENCES `myrefappusers` (`user_id`);
 
 --
--- Megkötések a táblához `myrefapplang`
+-- Constraints for table `myrefapplang`
 --
 ALTER TABLE `myrefapplang`
   ADD CONSTRAINT `myrefapplang_ibfk_1` FOREIGN KEY (`lang_uid`) REFERENCES `myrefappusers` (`user_id`);
 
 --
--- Megkötések a táblához `myrefappnews`
+-- Constraints for table `myrefappnews`
 --
 ALTER TABLE `myrefappnews`
   ADD CONSTRAINT `myrefappnews_ibfk_1` FOREIGN KEY (`myrefappnewsuserid`) REFERENCES `myrefappusers` (`user_id`);
 
 --
--- Megkötések a táblához `myrefapppersonaldesc`
+-- Constraints for table `myrefapppersonaldesc`
 --
 ALTER TABLE `myrefapppersonaldesc`
   ADD CONSTRAINT `myrefapppersonaldesc_ibfk_1` FOREIGN KEY (`pd_uid`) REFERENCES `myrefappusers` (`user_id`);
 
 --
--- Megkötések a táblához `myrefapppersonalinf`
+-- Constraints for table `myrefapppersonalinf`
 --
 ALTER TABLE `myrefapppersonalinf`
   ADD CONSTRAINT `myrefapppersonalinf_ibfk_1` FOREIGN KEY (`pi_uid`) REFERENCES `myrefappusers` (`user_id`);
 
 --
--- Megkötések a táblához `myrefappusers`
+-- Constraints for table `myrefappusers`
 --
 ALTER TABLE `myrefappusers`
   ADD CONSTRAINT `myrefappusers_ibfk_1` FOREIGN KEY (`myrefrole_id`) REFERENCES `myrefapproles` (`myrefapprole_id`);
 
 --
--- Megkötések a táblához `myrefappworkp`
+-- Constraints for table `myrefappworkp`
 --
 ALTER TABLE `myrefappworkp`
   ADD CONSTRAINT `myrefappworkp_ibfk_1` FOREIGN KEY (`wp_uid`) REFERENCES `myrefappusers` (`user_id`);
